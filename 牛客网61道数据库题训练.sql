c7f94d30a08f6c4faf7cd72e08a3f52406b48409
Cliff-Ford <31761320+Cliff-Ford@users.noreply.github.com>
2019-04-03 18:44:39 +0800

    2019/4/3做题

--- 牛客网61道数据库题训练.sql
+++ 牛客网61道数据库题训练.sql
@@ -157,15 +157,177 @@ order by salary desc
 limit 1,1;
 
 18.查找当前薪水(to_date='9999-01-01')排名第二多的员工编号emp_no、薪水salary、last_name以及first_name，不准使用order by
-select count(distinct emp_no) n from salaries where salaries.to_date = '9999-01-01'
-
-select s.emp_no, max(s.salary) salary, e.last_name, e.first_name
-from salaries s
+select c.emp_no, c.salary, e.last_name, e.first_name
+from
+	(select b.emp_no, max(s.salary) salary
+	from
+		(select e.emp_no, e.last_name, e.first_name
+		from employees e
+		left join
+			(select employees.emp_no from employees 
+			join salaries on employees.emp_no = salaries.emp_no 
+			where salaries.to_date = '9999-01-01' and salaries.salary = (select max(salary) from salaries where salaries.to_date = '9999-01-01')) a
+		on e.emp_no = a.emp_no
+		where a.emp_no is null) b
+	join salaries s
+	on b.emp_no = s.emp_no
+	where s.to_date = '9999-01-01'
+	group by b.emp_no
+    order by salary desc
+	limit 0,1) c
 join employees e
-on s.emp_no = e.emp_no
-where s.to_date = '9999-01-01'
-group by s.emp_no
-having salary != (select max(salaries.salary) from salaries where salaries.to_date = '9999-01-01')
+on c.emp_no = e.emp_no;
+
+CREATE TABLE `departments` (
+`dept_no` char(4) NOT NULL,
+`dept_name` varchar(40) NOT NULL,
+PRIMARY KEY (`dept_no`));
+
+19.查找所有员工的last_name和first_name以及对应的dept_name，也包括暂时没有分配部门的员工
+select e.last_name, e.first_name, d.dept_name
+from employees e
+left join dept_emp de
+on e.emp_no = de.emp_no
+left join departments d
+on de.dept_no = d.dept_no;
+
+20.查找员工编号emp_no为10001其自入职以来的薪水salary涨幅值growth，只能从salaries表中获取数据
+select (
+	(select salary from salaries where emp_no = '10001' order by to_date desc limit 0,1)
+    -
+    (select salary from salaries where emp_no = '10001' order by to_date asc limit 0,1)
+) growth;
+
+21.查找所有员工自入职以来的薪水涨幅情况，给出员工编号emp_no以及其对应的薪水涨幅growth，并按照growth进行升序(题目有坑，题目说是所有员工，但如果去掉‘9999-01-01’这个条件限制就不通过)
+
+select b.emp_no, (b.salary-c.salary) growth
+from
+	(select s.emp_no, s.salary
+	from
+		 salaries s
+	join (select s.emp_no, max(s.to_date) to_date from salaries s where s.to_date = '9999-01-01' group by s.emp_no) a 
+	on s.emp_no = a.emp_no and s.to_date = a.to_date) b
+join
+	(select s.emp_no, s.salary from salaries s join employees e on s.emp_no = e.emp_no where s.from_date = e.hire_date) c
+on b.emp_no = c.emp_no
+order by growth;
+
+22.统计各个部门对应员工涨幅的次数总和，给出部门编码dept_no、部门名称dept_name以及次数sum
+select 
+	d.dept_no, d.dept_name, count(*) sum
+from
+	departments d, dept_emp de, salaries s
+where
+	d.dept_no = de.dept_no and de.emp_no = s.emp_no
+group by 
+	d.dept_no;
+或者
+select
+    d.dept_no, d.dept_name, count(s.salary) sum
+from
+    departments d
+join
+    (dept_emp de join salaries s on de.emp_no = s.emp_no)
+on
+    d.dept_no = de.dept_no
+group by
+    d.dept_no, d.dept_name
+order by
+	d.dept_no;
+	
+23.对所有员工的当前(to_date='9999-01-01')薪水按照salary进行按照1-N的排名，相同salary并列且按照emp_no升序排列
+
+select s1.emp_no, s1.salary, count(distinct s2.salary) as rank
+from salaries s1, salaries s2
+where s1.to_date = '9999-01-01' and s2.to_date = '9999-01-01' and s1.salary <= s2.salary
+group by s1.emp_no, s1.salary
+order by rank, s1.emp_no;
+
+24.获取所有非manager员工当前的薪水情况，给出dept_no、emp_no以及salary ，当前表示to_date='9999-01-01'
+select d.dept_no, b.* 
+from
+	(select s.emp_no, s.salary 
+	from
+		(select e.emp_no from employees e left join dept_manager dm on e.emp_no = dm.emp_no where dm.emp_no is null) a
+	join salaries s
+	on a.emp_no = s.emp_no and s.to_date = '9999-01-01') b
+join dept_emp d
+on b.emp_no = d.emp_no;
+
+25.获取员工其当前的薪水比其manager当前薪水还高的相关信息，当前表示to_date='9999-01-01',
+结果第一列给出员工的emp_no，
+第二列给出其manager的manager_no，
+第三列给出该员工当前的薪水emp_salary,
+第四列给该员工对应的manager当前的薪水manager_salary
+select b.emp_no, b.manager_no, s.salary emp_salary, b.manager_salary
+from
+	(select a.*, de.emp_no
+	from
+		(select salary manager_salary, dm.emp_no manager_no, dm.dept_no
+		from dept_manager dm 
+		join salaries s 
+		on dm.emp_no = s.emp_no 
+		where dm.to_date = '9999-01-01' and s.to_date = '9999-01-01') a 
+    join 
+		dept_emp de
+	on
+		a.dept_no = de.dept_no and a.manager_no != de.emp_no) b
+join salaries s
+on b.emp_no = s.emp_no and s.to_date = '9999-01-01' and s.salary > b.manager_salary;
+
+26.汇总各个部门当前员工的title类型的分配数目，结果给出部门编号dept_no、dept_name、其当前员工所有的title以及该类型title对应的数目count
+
+select d.dept_no, d.dept_name, t.title, count(t.title) count
+from departments d, dept_emp de, titles t
+where d.dept_no = de.dept_no and de.emp_no = t.emp_no and t.to_date = '9999-01-01' and de.to_date = '9999-01-01'
+group by d.dept_no, d.dept_name, t.title
+order by d.dept_no;
+
+27.给出每个员工每年薪水涨幅超过5000的员工编号emp_no、薪水变更开始日期from_date以及薪水涨幅值salary_growth，并按照salary_growth逆序排列。
+提示：在sqlite中获取datetime时间对应的年份函数为strftime('%Y', to_date)
+select s1.emp_no, s1.from_date, (s2.salary-s1.salary) salary_growth
+from salaries s1, salaries s2
+where 
+	s1.emp_no = s2.emp_no 
+and (strftime('%Y', s1.to_date) = strftime('%Y', s2.to_date)-1 
+		or 
+	 strftime('%Y', s1.from_date) = strftime('%Y', s2.from_date)-1)
+and
+	s2.salary > s1.salary + 5000
+order by salary_growth desc;
+
+
+CREATE TABLE IF NOT EXISTS film (
+film_id smallint(5)  NOT NULL DEFAULT '0',
+title varchar(255) NOT NULL,
+description text,
+PRIMARY KEY (film_id));
+
+CREATE TABLE category  (
+category_id  tinyint(3)  NOT NULL ,
+name  varchar(25) NOT NULL, `last_update` timestamp,
+PRIMARY KEY ( category_id ));
+
+
+CREATE TABLE film_category  (
+film_id  smallint(5)  NOT NULL,
+category_id  tinyint(3)  NOT NULL, `last_update` timestamp);
+28.查找描述信息中包括robot的电影对应的分类名称以及电影数目，而且还需要该分类对应电影数量>=5部 (应该是题目有误)
+SELECT c.name, COUNT(fc.film_id) FROM
+ (select category_id, COUNT(film_id) AS category_num FROM
+     film_category  GROUP BY category_id HAVING count(film_id)>=5) AS cc,
+ film AS f, film_category AS fc, category AS c
+WHERE  f.description LIKE '%robot%'
+AND f.film_id = fc.film_id
+AND c.category_id = fc.category_id
+AND c.category_id=cc.category_id
+
+
+
+
+
+
+
 
 
 
